403a40b8de143c97f0003ed85c38bc5c062bdc62
Ruben Dominguez <rubendo99.9@hotmail.com>
2026-01-01 16:50:10 -0600

    testing more changes to website, adding blog functionality

--- build-site.el
+++ build-site.el
@@ -1,28 +1,54 @@
 (require 'ox-publish)
 
-;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
+;; Global HTML Settings (Applying your Simple.css preference)
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
-;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "my-org-site"
+       ;; --- Component 1: The Blog Posts ---
+       (list "blog"
+             :base-directory "./content/blog"
+             :base-extension "org"
+             :publishing-directory "./public/blog"
              :recursive t
-             :base-directory "./content"
-             :publishing-directory "./public"
              :publishing-function 'org-html-publish-to-html
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :auto-sitemap t
+             :sitemap-title "Articles & Thoughts"
+             :sitemap-filename "archive.org"
+             :sitemap-sort-files 'anti-chronologically
+             :with-author nil
+             :with-creator t
+             :with-toc t
+             :section-numbers nil
+             :time-stamp-file nil)
 
-;; Generate the site output
+       ;; --- Component 2: Static Pages (index.org) ---
+       (list "pages"
+             :base-directory "./content/"
+             :base-extension "org"
+             :exclude "blog/"
+             :publishing-directory "./public/"
+             :recursive nil
+             :publishing-function 'org-html-publish-to-html
+             :with-author nil
+             :with-creator t
+             :with-toc nil ;; Set to nil for a cleaner landing page
+             :section-numbers nil
+             :time-stamp-file nil)
+
+       ;; --- Component 3: Assets ---
+       (list "assets"
+             :base-directory "./content/"
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf"
+             :publishing-directory "./public/"
+             :recursive t
+             :publishing-function 'org-publish-attachment)
+
+       ;; Combined Project
+       (list "website" :components '("blog" "pages" "assets"))))
+
 (org-publish-all t)
-
 (message "Build complete!")
-
-
